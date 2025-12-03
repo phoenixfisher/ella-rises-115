0b2ff7d06719c7e11586663939e756ce42f8ff35
Phoenix Fisher <phoenixfisher702@gmail.com>
2025-12-03 09:55:29 -0700

    fix: env variable naming

--- src/server.js
+++ src/server.js
@@ -13,11 +13,11 @@ const app = express();
 const knex = require("knex")({
     client: "pg",
     connection: {
-        host: process.env.PGHOST,
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        port: process.env.PGPORT,
+        host: process.env.PG_HOST,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
+        port: process.env.PG_PORT,
     }
 });
 
