a514ff72254c80f2e9db86001cfbea59701d9213
Phoenix Fisher <phoenixfisher702@gmail.com>
2025-12-04 19:43:29 -0700

    fix EB env variable names

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ const knex = require("knex");
 const db = knex({
     client: "pg",
     connection: {
-        host: process.env.RDS_HOST,
-        user: process.env.RDS_USER,
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
-        database: process.env.RDS_DATABASE,
+        database: process.env.RDS_DB_NAME,
         port: process.env.RDS_PORT,
         ssl: process.env.RDS_SSL === "true" ? { rejectUnauthorized: false } : false,
     },
