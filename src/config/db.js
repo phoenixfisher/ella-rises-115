5fc32958b2d9b71876dde504960ab239847f2802
Phoenix Fisher <phoenixfisher702@gmail.com>
2025-12-04 20:35:34 -0700

    feat: console.log

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,13 @@
 const knex = require("knex");
 
+console.log("EB DB CONFIG:", {
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  database: process.env.RDS_DB_NAME,
+  port: process.env.RDS_PORT,
+  ssl: process.env.RDS_SSL
+});
+
 const db = knex({
     client: "pg",
     connection: {
